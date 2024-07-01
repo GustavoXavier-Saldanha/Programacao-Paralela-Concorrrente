2b15f781c27e54bb152a2bf9f57c534f10ac8101
Gustavo Xavier Saldanha <gustavosaldxav@gmail.com>
2024-07-01 20:17:45 -0300

    Melhoria na função da questão 11

--- Exercícios C/Laboratorios/Recuperacao/questao-11/main.c
+++ Exercícios C/Laboratorios/Recuperacao/questao-11/main.c
@@ -5,13 +5,15 @@
 #define TAM_ARR 10
 
 const int arr[TAM_ARR] = {5, 7, 7, 8, 8, 5, 1, 7, 7777777, 0};
-int count = 0;
 int num;
 int num_threads;
 pthread_mutex_t lock;
 
 void *count_occurrences(void *arg) {
-    int thread_id = *(int *)arg;
+    int *local_count = (int *)arg;
+    *local_count = 0;
+
+    int thread_id = *((int *)arg + 1);
     int start = (TAM_ARR / num_threads) * thread_id;
     int end = (TAM_ARR / num_threads) * (thread_id + 1);
     if (thread_id == num_threads - 1) {
@@ -20,20 +22,18 @@ void *count_occurrences(void *arg) {
 
     for (int i = start; i < end; i++) {
         if (arr[i] == num) {
-            pthread_mutex_lock(&lock);
-            count++;
-            pthread_mutex_unlock(&lock);
+            (*local_count)++;
         }
     }
+
     pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-
     num_threads = atoi(argv[1]);
 
     pthread_t threads[num_threads];
-    int thread_ids[num_threads];
+    int thread_data[num_threads][2];
 
     printf("Numero de threads utilizadas no sistema: %d\n\n", num_threads);
     printf("Numero a ser contabilizado: ");
@@ -42,17 +42,20 @@ int main(int argc, char *argv[]) {
     pthread_mutex_init(&lock, NULL);
 
     for (int i = 0; i < num_threads; i++) {
-        thread_ids[i] = i;
-        pthread_create(&threads[i], NULL, count_occurrences, (void *)&thread_ids[i]);
+        thread_data[i][0] = 0;
+        thread_data[i][1] = i;
+        pthread_create(&threads[i], NULL, count_occurrences, (void *)&thread_data[i]);
     }
 
+    int total_count = 0;
     for (int i = 0; i < num_threads; i++) {
         pthread_join(threads[i], NULL);
+        total_count += thread_data[i][0];
     }
 
     pthread_mutex_destroy(&lock);
 
-    printf("O numero %d ocorre %d vezes no array.\n", num, count);
+    printf("O numero %d ocorre %d vezes no array.\n", num, total_count);
 
     return 0;
 }
